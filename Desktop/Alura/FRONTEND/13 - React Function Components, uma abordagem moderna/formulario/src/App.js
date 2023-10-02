3ef1ececbf9638b58e06aa8cac780a216c1d90e6
wrbrito <130423510+wrbrito@users.noreply.github.com>
2023-10-02 18:52:11 -0300

    upada

--- Desktop/Alura/FRONTEND/13 - React Function Components, uma abordagem moderna/formulario/src/App.js
+++ Desktop/Alura/FRONTEND/13 - React Function Components, uma abordagem moderna/formulario/src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
+import { Container, Typography } from '@mui/material';
 class App extends Component {
-
     render(){
         return (
-            <h1>Formulario de cadastro</h1>
+            <Container component="article" maxWidth="sm">
+                <Typography variant='h3' component='h1' align='center'>Formulario de cadastro</Typography>
+                <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+            </Container>
         );
     }
-    
+}
+
+function aoEnviarForm(dados){
+    console.table(dados);
+}
+
+function validarCPF(cpf) {
+    if (cpf.length !== 11){
+        return {valido:false, texto:"CPF deve ter 11 digitos!!"}
+    }else{
+        return {valido:true, texto:""}
+    }
 }
 
 export default App;
